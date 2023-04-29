f0424d6cf0d441e67c942e118a608cd3f5d6c5e1
Gabriel Alexsandro <gabrielalexsandro2011@gmail.com>
2023-04-29 05:37:40 -0300

    🔨 style code in another file

--- components/scroll-bar.js
+++ components/scroll-bar.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Slider } from "@miblanchard/react-native-slider";
 import { StyleSheet, AppRegistry, View, Text, Dimensions } from "react-native";
 import { CameraTargetContext } from '../context/piano-track-context';
+import styles from '../styles/slider';
 
 export default function SliderExample() {
     const {thumbWidth} = useContext(CameraTargetContext);
@@ -13,48 +14,11 @@ export default function SliderExample() {
     const trackMarks = [maximumValue/4, 2*maximumValue/4, 3*maximumValue/4, 4* maximumValue/4];
     const touchThumbSize = {width: thumbWidth, height: 40};
 
-    const styles = StyleSheet.create({
-        sliderContainer: {
-            flex: 0.2,
-            justifyContent: 'center'
-        },
-        slider: {
-            height: '100%',
-        },
-        track: {
-            borderRadius: 2,
-            height: '100%',
-        },
-        thumbContainer: {
-            alignItems: 'center',
-            height: 50,
-            justifyContent: 'center',
-            width: thumbWidth,
-            borderRadius: 3.7,
-            borderColor: '#023020',
-            borderWidth: 2.2,
-        },
-        trackMarkContainer: {
-            backgroundColor: '#FFBF00',
-            borderWidth: 2.3,
-            borderColor: '#FFBF00',
-            height: 40,
-            left: -2
-        },
-        thumbGrass: {
-            opacity: 0.73,
-            backgroundColor: '#228B22',
-            width: '100%',
-            height: '100%'
-        }
-    });
-
     const CustomThumb = () => (
-        <View style={styles.thumbContainer}>
+        <View style={[styles.thumbContainer, {width: thumbWidth}]}>
             <View style={styles.thumbGrass}/>
         </View>
     );
-
     
     const offsettingTrackMarks = (pos) => {
         octaveSliderWidth = (sliderWidth - thumbWidth) / 4;
--- styles/slider.js
+++ styles/slider.js
@@ -1,6 +1,11 @@
 import { StyleSheet } from "react-native";
+import { useContext } from "react";
+import { CameraTargetContext } from "../context/piano-track-context";
 
-const THUMB_WIDTH = 50;
+// function getThumbWidth() {
+//     const {thumbWidth} = useContext(CameraTargetContext);
+//     return thumbWidth;
+// }
 
 const styles = StyleSheet.create({
     sliderContainer: {
@@ -16,17 +21,26 @@ const styles = StyleSheet.create({
     },
     thumbContainer: {
         alignItems: 'center',
-        backgroundColor: 'red',
         height: 50,
         justifyContent: 'center',
-        width: THUMB_WIDTH,
-        borderRadius: 5,
+        width: 50,
+        borderRadius: 3.7,
+        borderColor: '#023020',
+        borderWidth: 2.2,
     },
     trackMarkContainer: {
-        backgroundColor: 'green',
-        borderWidth: 4,
-        borderColor: 'green',
-        height: 70,
+        backgroundColor: '#FFBF00',
+        borderWidth: 2.3,
+        borderColor: '#FFBF00',
+        height: 40,
         left: -2
+    },
+    thumbGrass: {
+        opacity: 0.73,
+        backgroundColor: '#228B22',
+        width: '100%',
+        height: '100%'
     }
-});
+})
+
+export default styles;
